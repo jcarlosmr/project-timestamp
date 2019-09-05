58f6d7d8ad2a86f04f114b1d2b55ce70324029e6
Juan Carlos Mendoza <juan.mendoza@jmcxpress.com>
2019-09-04 22:31:32 -0500

    initial commit

---
+++ server.js
@@ -0,0 +1,59 @@
+// server.js
+// where your node app starts
+
+// init project
+const express = require('express');
+const app = express();
+
+// enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
+// so that your API is remotely testable by FCC
+const cors = require('cors');
+app.use(cors({ optionSuccessStatus: 200 })); // some legacy browsers choke on 204
+
+// http://expressjs.com/en/starter/static-files.html
+app.use(express.static('public'));
+
+// http://expressjs.com/en/starter/basic-routing.html
+app.get('/', function(req, res) {
+  res.sendFile(__dirname + '/views/index.html');
+});
+
+// your first API endpoint...
+app.get('/api/hello', function(req, res) {
+  res.json({ greeting: 'hello API' });
+});
+
+// Code for date microservice
+app.get('/api/timestamp', function(req, res) {
+  let date = new Date();
+  res.json({
+    date
+  });
+});
+
+app.get('/api/timestamp/:date_string', function(req, res) {
+  const date_string = req.params.date_string;
+  let ts = Number(date_string);
+  if (!ts) {
+    date = new Date(date_string);
+  } else {
+    date = new Date(ts);
+  }
+  let utc = date.toUTCString();
+  let unix = date.getTime();
+  if (utc == 'Invalid Date') {
+    res.json({
+      error: 'Invalid Date'
+    });
+  } else {
+    res.json({
+      unix,
+      utc
+    });
+  }
+});
+
+// listen for requests :)
+const listener = app.listen(process.env.PORT, function() {
+  console.log('Your app is listening on port ' + listener.address().port);
+});
